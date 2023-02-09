a85c94b069c85d5b25f9c81646bea708d676322c
RIC217 <melericcardo9400@gmail.com>
2023-02-09 11:27:02 +0100

    Finished login window

--- main.v
+++ main.v
@@ -13,6 +13,11 @@ struct App {
 	pseudo_text_box &ui.TextBox
 	pseudo_text string
 	pseudo_is_error bool
+	password_text string
+	password_is_error bool
+	socket &net.TcpConn
+	addr string
+	port int
 }
 
 fn main() {
@@ -21,6 +26,11 @@ fn main() {
 		pseudo_text_box: 0
 		pseudo_text: ""
 		pseudo_is_error: true
+		password_text: ""
+		password_is_error: true
+		socket: 0
+		addr: ""
+		port: 0
 	}
 
 	app.window = ui.window(
@@ -34,23 +44,33 @@ fn main() {
 			ui.column(
 				alignment: .center
 				margin_: 16
+				widths: ui.stretch
+				spacing: 16
 				children: [
-					ui.row(
-						alignment: .center
-						widths: [ui.fit]
-						children: [
-							ui.label(
-								text: "Se connecter"
-								text_align: .center
-								text_color: gx.rgb(255, 255, 255)
-							)
-						]
+					ui.label(
+						text: "Se connecter"
+						//text_align: .center
+						text_color: gx.rgb(255, 255, 255)
+						justify: [0.5, 0.5]
+						text_size: 22
 					)
 
 					ui.textbox(
 						placeholder: "Pseudo"
 						on_change: app.pseudo_changed
 						is_error: &app.pseudo_is_error
+					)
+
+					ui.textbox(
+						placeholder: "Mot de passe"
+						on_change: app.password_changed
+						is_error: &app.password_is_error
+						is_password: true
+					)
+
+					ui.button(
+						text: "Se connecter"
+						on_click: app.connect
 					)
 				]
 			)
@@ -60,28 +80,30 @@ fn main() {
 	ui.run(app.window)
 }
 
-fn connect(addr string) {
-	mut socket := net.dial_tcp(addr) or {
-		panic(err)
+fn (mut app App) connect(_ &ui.Button) {
+	if app.socket != 0 {
+		app.socket.close() or { panic(err) }
 	}
 
-	socket.set_read_timeout(time.infinite)
+	app.socket = net.dial_tcp(app.addr) or { panic(err) }
 
-	spawn listen_for_messages(mut socket)
+	app.socket.set_read_timeout(time.infinite)
+
+	spawn app.listen_for_messages()
 
 	for {
 		data := os.input("")
-		socket.write_string(data) or {
+		app.socket.write_string(data) or {
 			eprintln(err)
 			exit(-1)
 		}
 	}
 }
 
-fn listen_for_messages(mut socket &net.TcpConn) {
+fn (mut app App) listen_for_messages() {
 	for {
 		mut data := []u8{len: 1024}
-		socket.read(mut data) or {
+		app.socket.read(mut data) or {
 			eprintln(err)
 			break
 		}
@@ -93,5 +115,27 @@ fn listen_for_messages(mut socket &net.TcpConn) {
 
 fn (mut app App) pseudo_changed(it &ui.TextBox) {
 	app.pseudo_text = it.text
-	app.pseudo_is_error = app.pseudo_text.len < 8
+	app.pseudo_is_error = app.pseudo_text.len < 3
 }
+
+fn (mut app App) password_changed(it &ui.TextBox) {
+	app.password_text = it.text
+	app.password_is_error = app.password_text.len < 8
+}
+
+/*fn get_font_root_path() string {
+	$if linux {
+		return "/usr/share/fonts/truetype/*"
+	}
+	$if macos {
+		return "/System/Library/Fonts/*"
+	}
+	$if windows {
+		return "C:/windows/fonts"
+	}
+}
+
+fn get_font_paths() []string {
+	font_dir := get_font_root_path()
+	return os.glob("$font_dir/*.ttf") or {panic(err)}
+}*/
