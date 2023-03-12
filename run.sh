a0b589002e4c72c55147765e38821fc16e890b5a
RIC217 <melericcardo9400@gmail.com>
2023-03-12 18:32:43 +0100

    Added launch scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+rm -r build && mkdir build && v -g . -o build/Client_Sockets && ./build/Client_Sockets
---
+++ run_optimised.sh
@@ -0,0 +1,1 @@
+rm -r build && mkdir build && v -g -prod . -o build/Client_Sockets && ./build/Client_Sockets
